46252b26de50d877967cb857f5c3d366f2f3cdfb
Coen van Leeuwen <coen.vanleeuwen@tno.nl>
2019-01-22 17:05:21 +0100

    Fix docker image versions and demonstrate java client

---
+++ client/src/main/java/nl/tno/ignite/Main.java
@@ -0,0 +1,21 @@
+package nl.tno.ignite;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+
+public class Main {
+
+	private final static String CONFIG_URI = "https://raw.githubusercontent.com/coenvl/ignite-config/master/p2p/TcpDicoverySingleJobConfiguration.xml";
+	
+	public static void main(String [] args) {
+		Ignite ignite = Ignition.start(CONFIG_URI);
+        ignite.compute().broadcast(() -> System.out.println("Hello world!"));
+        ignite.compute().run(Main::findMe);
+        ignite.close();
+	}
+	
+	public static void findMe() {
+		System.out.println("Hello you!");
+	}
+	
+}
